377816428612688ce962d6afdb37e5fd0986e9a8
ObloqulovShoyim <obloqulovshoyim1@gmail.com>
2024-03-16 01:00:17 +0500

    admin template files fixed

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -19,14 +19,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
-    <link href="{{ asset('lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css') }}" rel="stylesheet" />
+    <link href="https://themewagon.github.io/darkpan/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="https://themewagon.github.io/darkpan/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
 
     <!-- Customized Bootstrap Stylesheet -->
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="https://themewagon.github.io/darkpan/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Template Stylesheet -->
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="https://themewagon.github.io/darkpan/css/style.css" rel="stylesheet">
 </head>
 
 <body>
